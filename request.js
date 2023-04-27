e780041a177c3768c352cb2fbacdcbf136aa699e
JumaboyevJasurbek <jumaboyevjasurbek2004@gmail.com>
2023-04-27 04:32:46 -0700

    request updated

--- request.js
+++ request.js
@@ -1,28 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
 async function downloaderMethod(inst_url) {
-	try {
-		const options = {
-			method: 'GET',
-			url: 'https://instagram-media-downloader.p.rapidapi.com/rapid/post.php',
-			params: { url: inst_url },
-			headers: {
-				'X-RapidAPI-Key': '34a74bb5d2mshbec3d8a4f434df8p135c31jsnad99779f9ffa',
-				'X-RapidAPI-Host': 'instagram-media-downloader.p.rapidapi.com',
-			},
-		};
+    try {
+        const options = {
+            method: "GET",
+            url: "https://instagram-media-downloader.p.rapidapi.com/rapid/post.php",
+            params: { url: inst_url },
+            headers: {
+                "content-type": "application/octet-stream",
+                "X-RapidAPI-Key":
+                    "8cc7597b66msh0bb1ff7bc5f5b7ap18ee75jsn901980db79c4",
+                "X-RapidAPI-Host": "instagram-media-downloader.p.rapidapi.com",
+            },
+        };
 
-		const response = await axios.request(options);
+        const response = await axios.request(options);
 
-		const result = {
-			videoUrl: response.data.video,
-			caption: response.data.caption,
-		};
+        const result = {
+            videoUrl: response.data.video,
+            caption: response.data.caption,
+        };
 
-		return result;
-	} catch (err) {
-		console.log(err);
-	}
+        return result;
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 export default downloaderMethod;
